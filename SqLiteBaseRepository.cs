172595b8cae4d50536c471f77622ba1340dd6f76
KeyurSoni-Upwork <85029261+KeyurSoni-Upwork@users.noreply.github.com>
2022-01-19 18:53:52 +0530

    Added outer class to handle SQLite Database
    SQLite Database handling class using ADO.Net.

---
+++ SqLiteBaseRepository.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Data.SQLite;
+using System.Diagnostics;
+using Dapper;
+namespace BoostDraftAlpha.CollFiltering.Controllers
+{
+  public  class SqLiteBaseRepository : IDisposable
+    {
+        public static string DbFile
+        {
+            get { return System.IO.Path.Combine(BoostDraftConfig.FolderPath, "BoostDefinitions.db"); }
+        }
+        public static SQLiteConnection SimpleDbConnection()
+        {
+            SQLiteConnection sqlcon = new SQLiteConnection();
+            try
+            {
+                sqlcon = new SQLiteConnection("Data Source=" + DbFile);
+                //Check weather connection is valid or not, only required for parallels/mac env? 
+                sqlcon.Open();
+                sqlcon.Close();
+            }
+            catch(Exception e)
+            {
+                var filename = "";
+                UsageLogger.AddError(filename, e);
+            }
+            return sqlcon;
+        }
+        public static void CreateDatabase()
+        {
+            using (var cnn = SimpleDbConnection())
+            {
+                try
+                {
+                    cnn.Open();
+                    cnn.Execute(@"create table IF NOT EXISTS DocumentFile
+                      (
+                         File_Id                            integer primary key AUTOINCREMENT,
+                         Filename                           nvarchar(100) not null,
+                         Path                               nvarchar(100) not null,
+                         Key                                nvarchar(100),
+                         Type                               nvarchar(100),
+                         Lang                               nvarchar(100) not null,
+                         Created_at                         datetime not null,
+                         Updated_at                         datetime not null
+                      )");
+                    cnn.Execute(@"create table IF NOT EXISTS DefinedTerm
+                      (
+                         Term_Id                             integer primary key AUTOINCREMENT,
+                         Term                                nvarchar(100) not null,
+                         Created_at                          datetime not null,
+                         Updated_at                          datetime not null
+                      )");
+                    cnn.Execute(@"create table IF NOT EXISTS FileandTerm
+                      (
+                         FileTermKey                         nvchar(100) primary key,
+                         File_Id                             integer not null,
+                         Term_Id                             integer not null,
+                         Description                         nvarchar(500),
+                         Updated_at                          datetime not null
+                      )");
+                    cnn.Execute(@"create table IF NOT EXISTS TermSimilarity
+                      (
+                         TermKey                             nvchar(100) primary key, 
+                         TermA_Id                            integer not null,
+                         TermB_Id                            integer not null,
+                         Similarity                          real not null,
+                         Updated_at                          datetime not null
+                      )");
+
+                    // Added query for Memo table
+                    cnn.Execute(@"create table IF NOT EXISTS Memo
+                      (
+                         File_ID                             int(10) primary key, 
+                         Section_No                          nvarchar(50) not null,
+                         Paragraph_Text                      nvarchar(max) not null,
+                         Original_Text                       nvarchar(max) not null,
+                         Content                             nvarchar(max) not null,
+                         Memo                                nvarchar(max) 
+                      )");
+                }
+                catch (Exception e)
+                {
+                    var filename = "";
+                    UsageLogger.AddError(filename, e);
+                }
+
+             }
+        }
+        public void Dispose()
+        {
+            SQLiteConnection.ClearAllPools();
+        }
+    }
+}
